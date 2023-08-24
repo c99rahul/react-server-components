7864cfdd206396a626a0c024ff406dcd7bdb5ac5
Rahul C <c99.rahul@gmail.com>
2023-08-24 17:09:29 +0530

    Added one more route to improve demoing

--- app/posts/page.js
+++ app/posts/page.js
@@ -1,14 +1,7 @@
-import { Suspense } from "react";
 import PostList from "../components/PostList/PostList";
 
-const Index = async () => {
-  return (
-    <>
-      <Suspense fallback={<h2 className="post-loader">Loading...</h2>}>
-        <PostList />
-      </Suspense>
-    </>
-  );
+const Index = () => {
+  return <PostList />;
 };
 
 export default Index;
