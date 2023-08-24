71553fdaf98889551b8fe091a6828adbfeec33cd
Rahul C <c99.rahul@gmail.com>
2023-08-24 16:58:42 +0530

    Updated the ServerComponent.js again

--- app/components/ServerComponent.js
+++ app/components/ServerComponent.js
@@ -2,8 +2,8 @@ const ServerComponent = () => {
   const data =
     "I'm a Server Component. View developer console and then the server console in your browser to notice the difference.";
   /*
-   * Since this is a server component, the below message
-   * won't be displayed in the browser's dev console.
+   * Since this is a server component, the below message-logging
+   * won't work in the browser's dev console.
    */
   console.log(data);
 
