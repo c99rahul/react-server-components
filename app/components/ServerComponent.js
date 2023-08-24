9c0fe4cc0c4f6b580012167ffa1814182e1705a6
Rahul C <c99.rahul@gmail.com>
2023-08-24 16:56:57 +0530

    Updated the ServerComponent.js

--- app/components/ServerComponent.js
+++ app/components/ServerComponent.js
@@ -1,6 +1,6 @@
 const ServerComponent = () => {
   const data =
-    "I'm a Server Component. View developer console and then original source in your browser to notice the difference.";
+    "I'm a Server Component. View developer console and then the server console in your browser to notice the difference.";
   /*
    * Since this is a server component, the below message
    * won't be displayed in the browser's dev console.
